8a80351c93ef578568781c9c87db058b6fbb5084
Anthony Wang <hugh1234@mail.ustc.edu.cn>
2015-10-30 12:30:45 +0800

    move new source here

---
+++ replace.hs
@@ -0,0 +1,60 @@
+import qualified Text.CSV(printCSV, csv)
+import Text.Parsec.Prim(ParsecT,Stream)
+import Text.ParserCombinators.Parsec
+import System.Console.GetOpt
+import System.IO
+import System.Exit
+import System.Environment
+import Data.List
+import Data.Char
+import Control.Monad
+import Text.Printf
+
+unquotedEscChar = '\\'
+cmdList = 
+        [
+            ["u", "ü"]
+        ,   ["o", "ö"]
+        ,   ["a", "ä"]
+        ,   ["U", "Ü"]
+        ,   ["O", "Ö"]
+        ,   ["A", "Ä"]
+        ,   ["s", "ß"]
+
+        ,   ["1", "⸚"]
+        ,   ["\\","\\"]
+        ,   ["#", "#"]
+        ]
+
+parseCmd ::String -> Parser String
+parseCmd escStr= try(
+           do
+           string escStr
+           cmd <- choice (fmap (string . head) cmdList)
+           return (processCmd cmd)
+           ) <?> "parseCmd"
+
+processCmd c = last (concat filList)
+            where filList = filter (\x -> head x == c ) cmdList
+
+field :: Parser String
+field = do xs <- many ( parseContent [[unquotedEscChar]] <|> parseCmd [unquotedEscChar] <?> "An unquoted Field")
+           return $ concat xs
+
+parseContent not= many1 $ notString not
+
+notString s =
+        do
+        notFollowedBy $ choice (fmap (try . string) s )
+        anyChar
+
+parseString f c = 
+        case parse field f c of
+            Left e -> do putStrLn "Error parsing input:"
+                         print e
+            Right r ->  putStr r
+
+
+main = do
+        args <- getArgs
+        mapM (parseString "" ) args 
